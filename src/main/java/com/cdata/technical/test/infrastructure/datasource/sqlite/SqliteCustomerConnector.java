78a72f198065b36183a7f3b1cc69faf79f646817
kaku10 <>
2024-02-12 13:28:04 +0900

    [feat]Implement `createTableIfNotExists`

--- src/main/java/com/cdata/technical/test/infrastructure/datasource/sqlite/SqliteCustomerConnector.java
+++ src/main/java/com/cdata/technical/test/infrastructure/datasource/sqlite/SqliteCustomerConnector.java
@@ -3,11 +3,21 @@ package com.cdata.technical.test.infrastructure.datasource.sqlite;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.List;
 
 public class SqliteCustomerConnector {
     private final Connection sqliteConn;
 
     public SqliteCustomerConnector(String sqliteJdbcUrl) throws SQLException {
         this.sqliteConn = DriverManager.getConnection(sqliteJdbcUrl);
+    }
+
+    public void createTableIfNotExists(String tableName, List<String> columns) throws SQLException {
+        String sql = "CREATE TABLE IF NOT EXISTS " + tableName + " (";
+        for (String column : columns) {
+            sql += column + " TEXT,";
+        }
+        sql = sql.substring(0, sql.length() - 1) + ")";
+        sqliteConn.createStatement().execute(sql);
     }
 }
